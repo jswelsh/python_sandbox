8e971c8b2b9d5ec1271ec8e856800e2c04b1beb4
jswelsh <james.sinclair.welsh@gmail.com>
2021-03-08 23:19:47 -0800

    .

---
+++ edabit/veryEasy.py
@@ -0,0 +1,27 @@
+def addition(a, b):
+	return a + b
+print(8, ':', addition(3, 5))
+
+def nextNumber(num):
+	return num + 1
+print(8, ':',nextNumber(7))
+
+def convertMinutesToSeconds(minutes):
+	return minutes * 60 
+print(180, ':',convertMinutesToSeconds(3))
+
+def areaOfTriangle(base, height):
+	return base * height / 2
+print(50.0, ':', areaOfTriangle(10,10))
+
+def ConvertAgetoDays(age):
+	return age * 365
+print(7300, ':', ConvertAgetoDays(20))
+
+def PerimeterRectangle(length, width):
+	return (length + width) * 2
+print(26, ':', PerimeterRectangle(6, 7))
+
+def MaximumEdgeTriangle(side1, side2):
+	return side1 + side2 - 1
+print(17, ':', MaximumEdgeTriangle(8,10))
